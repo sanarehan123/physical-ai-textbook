1b9960def2ac44a520a08dc3494e6fbb2dd7fdfc
sanarehan123 <sanarehan230@gmail.com>
2025-12-17 18:48:37 +0500

    Updates

--- assets/js/aba21aa0.7e75a5ce.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkbook=globalThis.webpackChunkbook||[]).push([[5742],{7093:o=>{o.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- assets/js/acecf23e.8838f03e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkbook=globalThis.webpackChunkbook||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
